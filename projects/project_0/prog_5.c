d56d32fe4d78252048f2e4893271a057e132b445
Yadu Raj Bhageria <yrb13@ic.ac.uk>
2016-01-25 15:39:28 +0000

    Minor changes to Project 0. Ready for Submission

--- projects/project_0/prog_5.c
+++ projects/project_0/prog_5.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main(void) {
 	int high=160,step=2,F,C=-40;
-	printf("     C          K      F          R\n");
-	while(C <= high)
-	{
+	printf("     C          K         F          R\n");
+	while(C <= high){
 		F=32+9*C/5;
-		printf("%6d %10.2f %6.1f %10.2f \n", C,C+273.15,(float)F,F+459.67);
+		printf("%6d %10.2f %9.1f %10.2f \n", C,C+273.15,(float)F,F+459.67);
 		C=C+step;
 	}
 	return(0);
