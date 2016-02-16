996c894afb0d2561a5546239d3b35546ff515ce9
Yadu Raj Bhageria <yrb13@ic.ac.uk>
2016-02-16 21:02:06 +0000

    Project 1b: completed redone part 2

--- projects/project_1b/rcubic_roots.c
+++ projects/project_1b/rcubic_roots.c
@@ -5,115 +5,80 @@
 #include "quad_roots.h"
 #include "rcubic_roots.h"
 
-double rcubic_value(double a2, double a1, double a0, double x){
-    return (x*x*x + a2*x*x + a1*x + a0);
-}
-double diffrcubic_value(double a2, double a1, double a0, double x){
-    double three=3,two=2;
-    return (three*x*x + two*a2*x + a1);
-}
+void set_starting_y0(double, double *);
+double iterate_y(double, double );
+double newton_rapheson(double );
+int roots_return(double, double, double, double *, double *, double *);
+int rcubic_roots(double, double, double, double *, double *, double *);
 
+/* Cubic Solver using the Newton-Rapheson approximation for the first real root */
 int rcubic_roots(double a2, double a1, double a0, double *r1, double *r2, double *r3){
     double zero=0,one=1,two=2,three=3;
-    double alpha,beta,p,q,r,y0,yn,yn_plus1,yn_div=1e100;
-    int count,quad_case;
+    double alpha,beta,p,y;
 
-// for the case x^3 = 0
     if (a2 == zero & a1 == zero & a0 == zero) {
+/* a2=a1=a0=0. So x^3 = 0 => r1=r2=r3=0 */
         *r1=zero;
         return(1);
-    } else if (a1 == zero & a0 == zero){
+    }
+    beta=-a2/three;
+    alpha=cbrt(two*beta*beta*beta - a1*beta - a0); //can change to pow(z,1/3) if not using c11
+    if (a0==zero){
+/*trivial case where we only need to solve a quadratic equation
+of the form x(x^2 + a2*x + a1) = 0 */
         *r1=zero;
-        *r2=zero;
-        *r3=-a2;
-        order_2(r1,r3);
-        return(2);
-    } else if (a0==zero) {
-        *r1=zero;
-        quad_case = quad_roots(one,a2,a1,r2,r3);
-        switch (quad_case) {
-            case 0: return(0);
-            case 1: if (r1==r2) {return(1);} else {return(2);}
-            case 2: order_3(r1,r2,r3); if (r1==r2){return(2);} else if (r2==r3){return(2);} else {return(3);}
-            /* In case 2 r1 cannot equal r3 as r1,r2,r3 are ordered and hence we would have dealt with that in case 1 */
-        }
-    } else if (a0==a1*a2) {
-        *r1=-a2;
-        if (a1<zero){ //real roots
-            *r2=sqrt(-a1);
-            *r3=-sqrt(-a1);
-            order_3(r1,r2,r3);
-            //add case where one of roots is equal to r1.
-            return(3);
-        } else{ //imaginary roots
-            *r2=zero; //real part
-            *r3=sqrt(a1); //imaginary part
-            return(0);
-        }
-    } else{
-        //Now we reduce the cubic to the form y^3 - 3py - 1 = 0 and deal with that
-        q=(a2*a2-three*a1)/9.0;
-        r=(2.0*a2*a2*a2-9.0*a2*a1+27.0*a0)/54.0;
-        beta=-a2/three;
-        /* Cases to Deal with */
-        if (q==zero){ // !NOT NEEDED!
-            *r1=cbrt(-two*r)+beta; //real root
-            *r2=-cbrt(-two*r)/two + beta; //real part of complex root
-            *r3=sqrt(three)*fabs(cbrt(-two*r))/two; //imaginary part of complex root
-            return(0);
-        } else if (r==zero) { // !NOT NEEDED!
-            *r1=-a2/three;
-            if (q<zero) { //imaginary case
-                *r2=*r1;
-                *r3=sqrt(-three*q);
-                return(0);
-            } else { //real roots
-                //as we know q=/=0 we can simply set the roots as we need
-                *r2=*r1;
-                *r1=*r1+sqrt(three*q);
-                *r3=*r1-sqrt(three*q);
-                return(3);
-            }
-        } else if (q==zero & r==zero){
-            *r1=beta;
-            return(1);
-        } else if (q*q*q==r*r) { // !NOT NEEDED!
-            //implied that q>0
-            *r1=-a2-two*(r/q + beta);
-            *r2=r/q + beta;
-            *r3=*r2;
-            order_2(r1,r3);
-            return(2);
-        }
-
-        alpha=cbrt(two*beta*beta*beta - a1*beta - a0); //can change to pow(z,1/3) if not using c11
+    }  else if (alpha == zero){
+        *r1 = beta;
+        return(roots_return(one,-two*beta,-two*beta*beta+a1,r1,r2,r3));
+    } else {
         p=(three*beta*beta-a1)/(alpha*alpha);
-        if (p==zero){yn=one;}
-        else {
-            if (p>11.0/three){y0=sqrt(p);} else if (p<-1.92) {y0=-1.0/p;} else {y0=1.0 + p/3.0 - (p*p*p)/81.0;} //set starting value of p
-            yn=y0;
-            /* printf("%g\n", yn_div); */
-            for (count=1;count<4;count=count+1) {
-                /* printf("count: %d\n", count); */
-                yn_plus1 = yn - rcubic_value(zero,-p,-one,yn)/diffrcubic_value(zero,-p,-one,yn);
-                /* printf("yn = %.10g, yn+1 = %.10g\n", yn,yn_plus1); */
-                if (yn_plus1==yn) {/*printf("yn+1 == yn\n");*/break;}
-                if (yn_div-fabs(yn-yn_plus1)<zero){/*printf("|yn+1 - yn| diverging\n");*/break;}
-                yn_div=fabs(yn-yn_plus1);
-                yn=yn_plus1;
-            }
-        } *r1= alpha*yn + beta;
-        /*printf("Note: one root found has been found using the Newtom Rapheson approximation method.\n");
-        One real root has been found. Now we use our quadratic formula
-        to find the other roots and return the appropriate value */
-        quad_case = quad_roots(one,a2+(*r1),-a0/(*r1),r2,r3);
-        switch (quad_case) {
-            case 0: return(0);
-            case 1: if (r1==r2){return(1);} else {return(2);}
-            case 2: order_3(r1,r2,r3); if (r1==r2){return(2);} else if (r2==r3){return(2);} else {return(3);}
-            /* In case 2 r1 cannot equal r3 as r1,r2,r3 are ordered and hence we would have dealt with that in case 1 */
+        y=newton_rapheson(p);
+        *r1= alpha*y + beta;
+    }
+    return(roots_return(one,a2+(*r1),-a0/(*r1),r1,r2,r3));
+}
+
+/* finds yn+1 for given yn and p */
+double iterate_y(double p, double y){
+    double one=1,three=3;
+    return(y-(y*y*y-p*y-one)/(three*y*y-p));
+}
+
+/* Sets starting value of yn based on p */
+void set_starting_y0(double p, double *y0){
+    if (p>11.0/3.0){*y0=sqrt(p);}
+    else if (p<-1.92) {*y0=-1.0/p;}
+    else {*y0=1.0 + p/3.0 - (p*p*p)/81.0;}
+}
+
+/* Computes first real root using the Newton-Rapheson approximation method */
+double newton_rapheson(double p){
+    double zero=0,one=1,yn,yn1,diff,diff1;
+    int count=1;
+    if (p==zero){yn1=one;}
+    else {
+        set_starting_y0(p,&yn);
+        yn1 = iterate_y(p,yn);
+        while (yn!=yn1 && ( count<4 || diff1 < diff )) {
+            yn=yn1;
+            yn1 = iterate_y(p,yn);
+            diff1 = fabs(yn-yn1);
+            diff = diff1;
+            count = count + 1;
+            /* printf("yn = %.10g, yn+1 = %.10g\n", yn,yn1); //to test */
         }
     }
-    printf("Error!\n");
-    return(-1); //error
+    return(yn1);
+}
+
+/* Computes r2 and r3 once r1 has been found. Returns an integer corresponding to the number of real roots found */
+int roots_return(double a2, double a1, double a0, double *r1, double *r2, double *r3){
+    int quad_case = quad_roots(a2,a1,a0,r2,r3);
+    switch (quad_case) {
+        case 0: return(0);
+        case 1: if (r1==r2){return(1);} else {return(2);}
+        case 2: order_3(r1,r2,r3); if (r1==r2){return(2);} else if (r2==r3){return(2);} else {return(3);}
+        /* In case 2 r1 cannot equal r3 as r1,r2,r3 are ordered and hence we would have dealt with that in case 1 */
+    }
+    return(-1); /* for errors */
 }
